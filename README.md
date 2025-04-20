375dfe7111a53e93bfdb0e905938f94b1b9d6afc
Shua-github <CHA_shua@outlook.com>
2025-04-20 12:47:53 +0800

    [自用](

--- README.md
+++ README.md
@@ -1,30 +1,4 @@
-# LiteloaderQQNT插件 - QQ协议包增强工具
+# 自用
 
-利用发送协议包实现 PC NTQQ 没有的功能，如 戳一戳，设置头衔
-
-核心代码来自 [NapCatQQ](https://github.com/NapNeko/NapCatQQ)
-
-## 支持的版本
-
-* Windows X64 QQ 28418 ~ 29456
-
-* Linux X64 QQ 28498 ~ 29456
-
-* Linux ARM64 QQ 28498 ~ 29456
-
-* Mac ARM64 QQ 29456 ~ 29456
-
-## 使用方法
-
-### 安装
-
-Release 页面下载并将本插件放入 LiteLoaderQQNT 插件目录即可, 重启QQ即可，或者参考 LiteloaderQQNT [插件安装方法](https://liteloaderqqnt.github.io/guide/plugins.html)
-
-注意：Mac 用户需要在 `设置 - 隐私与安全` 允许 MoeHoo 的运行，然后重启QQ
-
-
-### 功能
-
-群内对着群友头像右键即可显示功能菜单
-
-好友消息框对着好友头像右键发送戳一戳
+> [!TIP]
+> 可以修改 `src/main.ts` 文件中的 `serverUrl` 变量(
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,69 @@
+import http from 'http';
+import { BrowserWindow, ipcMain } from 'electron';
+import PromptManager from "./electron-prompts/src/index";
+import { PromptTemplate } from "./electron-prompts/src/types";
 
-// import prompt from 'custom-electron-prompt'
-import PromptManager from "./electron-prompts/src/index"
-const prompts = new PromptManager({width: 400})
+const prompts = new PromptManager({ width: 400 });
 
-import {NTQQPacketApi, initWrapperSession} from "./napcat-protocol-packet";
-import {BrowserWindow, ipcMain} from 'electron';
-import {PromptTemplate} from "./electron-prompts/src/types";
+const serverUrl = 'http://127.0.0.1:3000';
 
-let wrapperSession: any = undefined;
-let api: NTQQPacketApi;
-
-initWrapperSession().then((session: any) => {
-    console.log('initSession done', session);
-    wrapperSession = session;
-    api = new NTQQPacketApi(wrapperSession);
-});
-
-interface PokeParams{
+interface PokeParams {
     groupUin: string;
     userUin: string;
 }
 
-interface SpecialTitleParams{
+interface SpecialTitleParams {
     groupUin: string;
     userUid: string;
 }
 
-ipcMain.handle('llqqnt_pp_poke', async (event: any, {groupUin, userUin}: PokeParams) => {
+function sendHttpRequest(url: string, data: object): Promise<any> {
+    return new Promise((resolve, reject) => {
+        const req = http.request(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(JSON.stringify(data)),
+            }
+        }, (res) => {
+            let responseData = '';
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(responseData));
+                } catch (e) {
+                    reject(new Error('Invalid JSON response'));
+                }
+            });
+        });
+
+        req.on('error', (e) => {
+            reject(e);
+        });
+
+        req.write(JSON.stringify(data));
+        req.end();
+    });
+}
+
+ipcMain.handle('llqqnt_pp_poke', async (event: any, { groupUin, userUin }: PokeParams) => {
     console.log('llqqnt_pp_poke called', userUin, groupUin);
     try {
-        if (groupUin) {
-            await api.sendPokePacket(+userUin, +groupUin);
-        } else {
-            await api.sendPokePacket(+userUin);
-        }
-    }catch (e) {
+        const url = groupUin ? `${serverUrl}/group_poke` : `${serverUrl}/friend_poke`;
+        const data = {
+            user_id: userUin,
+            ...(groupUin && { group_id: groupUin }), // 如果有 groupUin 则包含 group_id
+        };
+        const response = await sendHttpRequest(url, data);
+        console.log('Poke response:', response);
+    } catch (e) {
         console.log('llqqnt_pp_poke error', e);
     }
 });
 
-ipcMain.handle('llqqnt_pp_setSpecialTitle', async (event: any, {groupUin, userUid}: SpecialTitleParams) => {
-    // let prompt = require('electron-osx-prompt');
-    // const title = await prompt('设置头衔', '');
+ipcMain.handle('llqqnt_pp_setSpecialTitle', async (event: any, { groupUin, userUid }: SpecialTitleParams) => {
     const pTemplate: PromptTemplate = {
         windowTitle: "",
         cancelButton: {
@@ -55,7 +77,7 @@ ipcMain.handle('llqqnt_pp_setSpecialTitle', async (event: any, {groupUin, userUi
             {
                 type: "input",
                 name: "title",
-                placeholder: "",
+                placeholder: "请输入群头衔",
                 value: "",
                 classes: ["form-control"],
             },
@@ -72,8 +94,16 @@ ipcMain.handle('llqqnt_pp_setSpecialTitle', async (event: any, {groupUin, userUi
     const title = (await prompts.spawn(pTemplate))?.values.title;
 
     console.log('llqqnt_pp_setSpecialTitle called', groupUin, userUid, title);
-    if (title){
-        await api.sendSetSpecialTittlePacket(groupUin, userUid, title);
+    try {
+        // 如果 title 为空，表示移除群头衔
+        const response = await sendHttpRequest(`${serverUrl}/set_group_special_title`, {
+            group_id: groupUin,
+            user_id: userUid,
+            special_title: title || "", // 空字符串表示去掉头衔
+        });
+        console.log('Set title response:', response);
+    } catch (e) {
+        console.log('llqqnt_pp_setSpecialTitle error', e);
     }
 });
 
@@ -81,7 +111,7 @@ export function onBrowserWindowCreated(window: BrowserWindow) {
     const allWindows = BrowserWindow.getAllWindows();
     allWindows.forEach((window) => {
         if (!window.isDestroyed()) {
-            window.webContents.send('llqqnt_pp_create_window', {windowId: window.id});
+            window.webContents.send('llqqnt_pp_create_window', { windowId: window.id });
         }
     });
 }
